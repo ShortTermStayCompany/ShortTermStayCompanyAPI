ca5d36d3d80bc5d79d8cfb93611005e07da72ef6
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-11-18 17:56:01 -0800

    switching port from 5000 to 8000

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ def create_app():
 
     return app
 
-if __name__ == '__main__':
-    app = create_app()
+app = create_app()  # Create the app at the module level
 
-    app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
+if __name__ == '__main__':
+    # app = create_app()
+
+    app.run(host="0.0.0.0", port=8000, debug=False)
