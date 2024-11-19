59a0141c7ebfcc65048e54e939ed703ab556af1f
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-11-18 17:03:02 -0800

    cors breaks azure web app?

--- secret_key_genp.py
+++
@@ -1,5 +0,0 @@
-import secrets
-
-# Generate a random secret key
-secret_key = secrets.token_hex(32)  # Generates a 64-character hex string (256 bits)
-print("Generated JWT Secret Key:", secret_key)
