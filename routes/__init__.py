86a4831206451484c99e2c4f3cea504fc519734a
emreutkan <127414322+emreutkan@users.noreply.github.com>
2024-11-29 00:20:29 -0800

    v1 test

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,13 +1,13 @@
 from .auth import auth_bp
 from .listing import listing_bp
 from .booking import booking_bp
-from .report import report_listings
-from .review import review_bp
 from .report import report_bp
+from .review import review_bp
 
 def init_app(app):
-    app.register_blueprint(auth_bp, url_prefix='/auth')
-    app.register_blueprint(listing_bp, url_prefix='/listing')
-    app.register_blueprint(booking_bp, url_prefix='/booking')
-    app.register_blueprint(review_bp, url_prefix='/review')
-    app.register_blueprint(report_bp, url_prefix='/report')
+    # Version 1 Blueprints
+    app.register_blueprint(auth_bp, url_prefix='/v1/auth')
+    app.register_blueprint(listing_bp, url_prefix='/v1/listing')
+    app.register_blueprint(booking_bp, url_prefix='/v1/booking')
+    app.register_blueprint(review_bp, url_prefix='/v1/review')
+    app.register_blueprint(report_bp, url_prefix='/v1/report')
